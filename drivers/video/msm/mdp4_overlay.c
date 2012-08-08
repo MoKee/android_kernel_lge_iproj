83e779396bc4aa88dfc7777a523add70bf6c488d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-08-09 00:29:22 +0100

    Revert "Free the ION buffer after use"
    This reverts commit 4161202db19d9543706f2c11d1444d13bfa3d3ae.

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2099,7 +2099,7 @@ void mdp4_overlay_pipe_free(struct mdp4_overlay_pipe *pipe)
 	iom_pipe_info = &mdp_iommu[pipe->mixer_num][pipe->pipe_ndx - 1];
 	iom_pipe_info->mark_unmap = 1;
 
-	mdp4_overlay_iommu_pipe_free(pipe->pipe_ndx, 1);
+	mdp4_overlay_iommu_pipe_free(pipe->pipe_ndx, 0);
 
 	memset(pipe, 0, sizeof(*pipe));
 
