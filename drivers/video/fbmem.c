473d0f0cb1c46c19eb06e8022236ee2e4d6c4665
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-10-21 03:14:48 +0100

    fbmem: Adjust clocks unsupported by the sii9244 MHL adapter
    Change-Id: Ic72b43b0af9627f193d2f09653a158db23b1d80a

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -1079,6 +1079,13 @@ static long do_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		if (!lock_fb_info(info))
 			return -ENODEV;
 		console_lock();
+
+#ifdef CONFIG_LGE_MHL_SII9244
+		if (var.reserved[3] == 16 || var.pixclock == 148500000) {
+			var.reserved[3] = 0;
+			var.pixclock = 74250000;
+		}
+#endif
 		info->flags |= FBINFO_MISC_USEREVENT;
 		ret = fb_set_var(info, &var);
 		info->flags &= ~FBINFO_MISC_USEREVENT;
